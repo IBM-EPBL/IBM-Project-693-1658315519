3a1aaa2cbfa42bdb59090f8253a7b68c31baed29
aarshana <61494068+aarshana@users.noreply.github.com>
2022-11-17 01:24:05 +0530

    Add files via upload

---
+++ Project development/Sprint 3/flask_website/app.py
@@ -0,0 +1,31 @@
+import numpy as np
+import os
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from flask import Flask,render_template,request
+
+app=Flask(__name__)
+
+model=load_model("digit.h5")
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/predict',methods=['GET','POST'])
+def upload():
+    if request.method=='POST':
+        f=request.files['image']
+        basepath=os.path.dirname(__file__)
+        filepath=os.path.join(basepath,'uploads',f.filename)
+        f.save(filepath)
+        img=image.load_img(filepath,target_size=(28,28))
+        x=image.img_to_array(img)
+        x=np.expand_dims(x,axis=0)
+        pred=np.argmax(model.predict(x),axis=1)
+        index=['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine']
+        text="The Classified Digit is : " +str(index[pred[0]])
+
+    return text
+if __name__=='__main__':
+    app.run(debug=False)
